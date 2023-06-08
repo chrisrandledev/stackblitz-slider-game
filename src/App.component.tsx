d7c04c591ff4619556ff888d383e459bee771a1c
chrisrandledev <117551889+chrisrandledev@users.noreply.github.com>
2023-06-08 14:17:37 +0100

    1 file modified, 1 added, 1 deleted

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -4,13 +4,17 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Welcome to the React.ts demo on StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <p>
-        <a href="https://medium.com/@thewebdevg/creating-a-gameboard-with-css-grid-47da8ac25078">
-          Source
-        </a>
-      </p>
+      <h1>React.js Slider Picture Puzzle</h1>
+      <div>
+        <h2>References</h2>
+        <ul>
+          <li>
+            <a href="https://medium.com/@thewebdevg/creating-a-gameboard-with-css-grid-47da8ac25078">
+              Medium article
+            </a>
+          </li>
+        </ul>
+      </div>
       <div className="game-board">
         <div className="wall"></div> <div className="wall"></div>
         <div className="wall"></div> <div className="wall"></div>
